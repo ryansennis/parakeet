1ba9babd33a27a80ee30c6f1867f8ba1d689909a
Ryan Ennis <ryansennis@hotmail.com>
2024-09-04 15:21:42 -0600

    requirements.txt

--- run_tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-# Discover and run all tests in the 'tests' directory
-loader = unittest.TestLoader()
-suite = loader.discover('tests')
-
-runner = unittest.TextTestRunner()
-runner.run(suite)
